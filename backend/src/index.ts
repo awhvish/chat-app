c0b304e0fe37f899cf0a8f7971a505ab29d14cb5
Avis Lowha <high.avis.lowha@gmail.com>
2025-01-20 17:42:47 +0530

    project build ready

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,11 +6,13 @@ import { connectDB } from "./lib/db";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import {app, server, io} from "./lib/socket";
+import path from "path";
 
 dotenv.config(); //for process.env. 
 
 
-const PORT = parseInt(process.env.PORT || "5000", 10);
+const PORT = parseInt(process.env.PORT || "5000");
+const __dirname = path.resolve();
 
 app.use(express.json()); //extract JSON Data from request.body
 app.use(cookieParser());
@@ -22,8 +24,15 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
+if (process.env.NODE_ENV == "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+}
 
-server.listen(PORT, '0.0.0.0', () => {
+app.get("*",  (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
     connectDB();
 })
