0fd47b9cc3dc67e6720ea85823a940796cfa7c14
Avis Lowha <high.avis.lowha@gmail.com>
2025-01-20 22:59:40 +0530

    node version change

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,13 +24,14 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-if (process.env.NODE_ENV == "production") {
+if (process.env.NODE_ENV != "development") {
+    console.log("on production:")
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
-    
+
 }
 server.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
